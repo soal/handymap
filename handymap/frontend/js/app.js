454c36fe548ceb09d124add62d868aeebda77eac
soal <sorrow.about.alice@gmail.com>
2016-03-17 22:21:10 +0300

    Init js app

--- handymap/frontend/js/app.js
+++ handymap/frontend/js/app.js
@@ -1,33 +1,14 @@
-var myTestObj = {
-  a: 1,
-  b: 2,
-  c: 3,
-  hello: {
-    en: "hello",
-    ru: "привет"
-  },
-  world: {
-    en: "world",
-    ru: "мир"
-  }
+var initialData = {
+  items: [
+    {assignee: 'Scott', text: 'Write a book about Marionette'},
+    {assignee: 'Andrew', text: 'Do some coding'}
+  ]
 };
 
-var GreetEn = `${myTestObj.hello.en} ${myTestObj.world.en}!`;
-var GreetRu = `${myTestObj.hello.ru}, ${myTestObj.world.ru}!`;
+var app = new Marionette.Application();
 
+app.on('start', function() {
+  Backbone.history.start();
+});
 
-var myTestFunc = function(one=1, two="testfunc", three=[1,2,3,4], four=[20, 30, 40, 50]) {
-  for (let i = 0; i < three.length; i++) {
-    three[i]  =`${three[i]}, ${one}, ${two}`;
-  }
-  console.log(three);
-
-  _.each(four, (elem) => console.log(elem+5));
-};
-
-window.onload = function() {
-  console.log("Hello from babel!");
-  console.log(GreetEn);
-  console.log(GreetRu);
-  myTestFunc();
-};
+app.start();
