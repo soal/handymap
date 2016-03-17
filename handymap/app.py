b5c6addd2de9ddad8278927194e875c4c224892e
soal <sorrow.about.alice@gmail.com>
2016-03-17 22:20:20 +0300

    Add some routes for test data

--- handymap/app.py
+++ handymap/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, send_file
 
 app = Flask(__name__)
 
@@ -6,6 +6,16 @@ app.jinja_env.globals['static'] = (
     lambda filename: url_for('static', filename = filename)
 )
 
+app.debug = True
+
 @app.route('/')
 def index():
   return render_template('index.html')
+
+@app.route('/events')
+def events():
+  return send_file('test_data/events.json', 'JSON')
+
+@app.route('/russia/<process>')
+def show_process(process):
+  return send_file('test_data/%s.json', 'JSON') % process
