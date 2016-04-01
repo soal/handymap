0233431ddecdc76aa84bae1d4d77645741a77ca5
soal <sorrow.about.alice@gmail.com>
2016-04-02 02:20:44 +0300

    Add top page menu layout

--- handymap/client/js/app.vue
+++ handymap/client/js/app.vue
@@ -1,8 +1,20 @@
 <template>
 <div id="app">
-  <h1>{{ hello }}</h1>
-  <router-view keep-alive>
-  </router-view>
+  <header id="top-menu">
+    <div id="brand" class="fixed-logo">
+      <a href="#" id="logo" class="main-logo label label-danger">HM</a>
+      <a v-show="menuShowed" @click="menuShowed = !menuShowed" href="javascript:;" class="angle-switcher up"><i class="fa fa-angle-double-up"></i></a>
+      <a v-show="!menuShowed" @click="menuShowed = !menuShowed" href="javascript:;" class="angle-switcher"><i class="fa fa-angle-double-down"></i></a>
+    </div>
+    <nav v-show="menuShowed" class="navbar navbar-dark bg-primary">
+      <ul class="nav navbar-nav">
+        <li class="nav-item">
+          <a href="/about" class="nav-link">About</a>
+         </li>
+      </ul>
+    </nav>
+   </header>
+  <router-view keep-alive></router-view>
 </div>
 </template>
 
@@ -15,6 +27,21 @@ export default {
   store,
   components: {
     BaseMap
+  },
+  vuex: {
+    getters: {
+      title: state => state.title
+    }
+  },
+  data: () => {
+    return {
+      menuShowed: true
+    }
+  },
+  methods: {
+    toggleMenu(event) {
+
+    }
   }
 }
 
