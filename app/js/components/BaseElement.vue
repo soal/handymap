a6fe4af70ccf21a751a4ab1fa5910d00ce7cdb8e
soal <sorrow.about.alice@gmail.com>
2016-07-13 01:38:56 +0300

    New components

---
+++ app/js/components/BaseElement.vue
@@ -0,0 +1,32 @@
+<template lang="html">
+  <div id="base-element">
+  </div>
+</template>
+
+<script>
+import elementsActions from "../actions/elementsActions";
+import InfoBox from "./InfoBox.vue";
+
+export default {
+  name: "BaseElement",
+  components: {
+    infoBox: InfoBox
+  },
+  vuex: {
+    getters: {
+      currentElement: state => state.currentElement,
+      elements: state => state.elements
+    },
+    actions: Object.assign(
+      elementsActions,
+      {}
+    )
+  },
+  ready() {
+    this.getElement(100, function({dispatch}, response) {
+      dispatch("SET_CURRENT_ELEMENT", response);
+      return response;
+    });
+  }
+};
+</script>
