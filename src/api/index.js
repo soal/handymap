18b435a78ad52c4370d7de309e8c5d9bf154725d
soal <sorrow.about.alice@gmail.com>
2017-05-07 21:17:19 +0300

    Moving api to async/swait syntax

--- src/api/index.js
+++ src/api/index.js
@@ -1,87 +1,77 @@
 import { API_ROOT } from '../config';
 
 export default {
-  root: () => {
-    return fetch(`${API_ROOT}/`)
-      .then(res => {
-        return res.json()
-      })
-      .catch(err => {
-        console.log(err);
-      })
+  root: async () => {
+    try {
+      let res = await fetch(`${API_ROOT}/`);
+      return await res.json();
+    } catch (err) {
+      console.log(err);
+    }
   },
 
-  dicts: () => {
-    return fetch(`${API_ROOT}/dicts`)
-      .then(res => {
-        return res.json().then(json => {
-          return json;
-        });
-      })
-      .catch(err => {
-        console.log(err);
-      })
+  dicts: async () => {
+    try {
+      let res = await fetch(`${API_ROOT}/dicts`);
+      return await res.json();
+    } catch (err) {
+      console.log(err);
+    }
   },
 
-  context: (id, nameSpace='public') => {
-    return fetch(`${API_ROOT}/${nameSpace}/contexts/${id}`)
-      .then(res => {
-        return res.json()
-      })
-      .catch(err => {
-        console.log(err);
-      })
+  context: async (id, nameSpace='public') => {
+    try {
+      let res = await fetch(`${API_ROOT}/${nameSpace}/contexts/${id}`);
+      return await res.json();
+    } catch (err) {
+      console.log(err);
+    }
   },
 
-  elements: (ids, nameSpace='public') => {
+  elements: async (ids, nameSpace='public') => {
     if (!ids instanceof Array) throw new Error('Ids must be an array');
-    return fetch(`${API_ROOT}/${nameSpace}/elements?ids=${ids.join(',')}`)
-      .then(res => {
-        return res.json()
-      })
-      .catch(err => {
-        console.log(err);
-      })
+    try {
+      let res = await fetch(`${API_ROOT}/${nameSpace}/elements?ids=${ids.join(',')}`);
+      return await res.json();
+    } catch (err) {
+      console.log(err);
+    }
   },
 
-  element: (id, nameSpace='public') => {
-    return fetch(`${API_ROOT}/${nameSpace}/elements/${id}`)
-      .then(res => {
-        return res.json()
-      })
-      .catch(err => {
-        console.log(err);
-      })
+  element: async (id, nameSpace='public') => {
+    try {
+      let res = await fetch(`${API_ROOT}/${nameSpace}/elements/${id}`);
+      return await res.json();
+    } catch (err) {
+      console.log(err);
+    }
   },
 
-  shapes: (ids, nameSpace='public') => {
+  shapes: async (ids, nameSpace='public') => {
     if (!ids instanceof Array) throw new Error('Ids must be an array');
-    return fetch(`${API_ROOT}/${nameSpace}/shapes?ids=${ids.join(',')}`)
-      .then(res => {
-        return res.json()
-      })
-      .catch(err => {
-        console.log(err);
-      })
+    try {
+      let res = await fetch(`${API_ROOT}/${nameSpace}/shapes?ids=${ids.join(',')}`);
+      return await res.json();
+    } catch (err) {
+      console.log(err);
+    }
   },
 
-  shape: (id, nameSpace='public') => {
-    return fetch(`${API_ROOT}/${nameSpace}/shapes/${id}`)
-      .then(res => {
-        return res.json()
-      })
-      .catch(err => {
-        console.log(err);
-      })
+  shape: async (id, nameSpace='public') => {
+    try {
+      let res = await fetch(`${API_ROOT}/${nameSpace}/shapes/${id}`);
+      return await res.json();
+    } catch (err) {
+      console.log(err);
+    }
   },
 
-  scenario: (name, nameSpace='public') => {
-    return fetch(`${API_ROOT}/${nameSpace}/scenarios/${name}`)
-      .then(res => {
-        return res.json()
-      })
-      .catch(err => {
-        console.log(err);
-      })
+  scenario: async (name, nameSpace='public') => {
+    try {
+      let res = await fetch(`${API_ROOT}/${nameSpace}/scenarios/${name}`);
+      return await res.json();
+    } catch (err) {
+      console.log(err);
+    }
   }
-}
+};
