4a6ce91a7d6d589ed824a0b1a9761e45eb670e7a
soal <sorrow.about.alice@gmail.com>
2017-07-22 22:10:36 +0300

    Codestyle changes

--- src/main.js
+++ src/main.js
@@ -1,22 +1,22 @@
-import Vue from 'vue';
-import mapboxgl from 'mapbox-gl';
-import { sync } from 'vuex-router-sync';
-import VueMapbox from 'vue-mapbox';
-import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
+import Vue from 'vue'
+import mapboxgl from 'mapbox-gl'
+import { sync } from 'vuex-router-sync'
+import VueMapbox from 'vue-mapbox'
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm'
 
-import store from './store';
-import router from './router';
+import store from './store'
+import router from './router'
 
-import App from './App.vue';
+import App from './App.vue'
 
-Vue.use(BootstrapVue);
-Vue.use(VueMapbox, { mapboxgl });
+Vue.use(BootstrapVue)
+Vue.use(VueMapbox, { mapboxgl })
 
-sync(store, router);
+sync(store, router)
 
 const app = new Vue({ // eslint-disable-line no-new, no-unused-vars
   store,
   router,
   render: (h) => h(App)
-}).$mount('#app');
+}).$mount('#app')
 
